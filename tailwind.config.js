20588d7f0b84da792b603f3edae44fe631a890cc
oyusti <oyusti@gmail.com>
2024-12-04 20:43:39 -0400

    modo dark configurable, configuracion de google fonts, fontawesome y tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'selector',
   content: [
     "./resources/**/*.blade.php",
     "./resources/**/*.js",
     "./resources/**/*.vue",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        'Montserrat': ['Montserrat', 'sans-serif']
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 }
 
